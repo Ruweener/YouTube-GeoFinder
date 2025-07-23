5131e0763c8e4ca93a8faedebdf5a3e495f0c6e7
Ruweener <ruween2120@gmail.com>
2025-07-23 00:31:55 -0400

    changed routing url

--- backend/index.js
+++ backend/index.js
@@ -15,23 +15,23 @@ app.use(express.static(path.join(__dirname, "../frontend/dist")));
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.GOOGLE_CLOUD_API_KEY;
 
-app.get("/api/youtube:lat,:lng,:radius", async (req, res) => {
-    const maxResults = 12;
-    const {lat, lng, radius} = req.params;
+app.get("/api/youtube", async (req, res) => {
+    const {lat, lng, radius, maxResults = 12} = req.query;
     const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&location=${lat},${lng}&locationRadius=${radius}km&maxResults=${maxResults}&type=video&key=${API_KEY}`;
 
     try {
         const response = await axios.get(url);
         res.json(response.data);
-    } catch(err) {
-        console.error(err.response? err.response.data : undefined || err.message);
+    } catch(error) {
+        console.error("Error fetching from YouTube API:", error.response ? error.response.data : error.message);
         res.status(500).json({error: "YouTube API request failed"})
     }
 });
 
-// app.get("/*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-// })
+// This is important for single-page applications (SPAs) like yours
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+})
 
 
 app.listen(PORT, () => {
